8a2ae37acaff9440e8a09aaab11c9c2f168c6f54
Kvcux <franekkru2013@gmail.com>
2026-03-22 08:57:08 +0100

    feat: shell command "castlefetch"

--- kernel.c
+++ kernel.c
@@ -3,6 +3,8 @@
 
 #define PROMPT     "CastleOS $ "
 #define PROMPT_LEN 11
+#define VERSION "1.0\n"
+#define KERNEL "KK x86\n"
 
 int strcmp(const char *a, const char *b) {
     while (*a && *b && *a == *b) { a++; b++; }
@@ -47,7 +49,10 @@ int main() {
                 print_color(" clears screen\n", MAKE_COLOR(COLOR_WHITE, COLOR_BLACK));
                 // - echo
                 print_color(" - echo:", MAKE_COLOR(COLOR_CYAN, COLOR_BLACK));
-                print_color(" prints expresion after 'echo'", MAKE_COLOR(COLOR_WHITE, COLOR_BLACK));
+                print_color(" prints expresion after 'echo'\n", MAKE_COLOR(COLOR_WHITE, COLOR_BLACK));
+                // - castlefetch
+                print_color(" - castlefetch:", MAKE_COLOR(COLOR_CYAN, COLOR_BLACK));
+                print_color(" show info about system", MAKE_COLOR(COLOR_WHITE, COLOR_BLACK));
             // clear
             } else if (strcmp(buffer, "clear") == 0) {
                 vga_clear(MAKE_COLOR(COLOR_WHITE, COLOR_BLACK));
@@ -55,6 +60,15 @@ int main() {
             } else if (strncmp(buffer, "echo ", 5) == 0) {
                 print_color("\n", MAKE_COLOR(COLOR_WHITE, COLOR_BLACK));
                 print_color(buffer + 5, MAKE_COLOR(COLOR_WHITE, COLOR_BLACK));
+            } else if (strcmp(buffer, "castlefetch") == 0) {
+                print_color("\n>=CastleOS=<\n", MAKE_COLOR(COLOR_YELLOW, COLOR_BLACK));
+                print_color("# # # # # # # # #      ", MAKE_COLOR(COLOR_GREEN, COLOR_BLACK)); print_color("OS: ", MAKE_COLOR(COLOR_MAGENTA, COLOR_BLACK)); print("CastleOS\n");
+                print_color("#################      ", MAKE_COLOR(COLOR_GREEN, COLOR_BLACK)); print_color("Version: ", MAKE_COLOR(COLOR_MAGENTA, COLOR_BLACK)); print(VERSION);
+                print_color("#CCCCCCCCCCCCCCC#      ", MAKE_COLOR(COLOR_GREEN, COLOR_BLACK)); print_color("Kernel: ", MAKE_COLOR(COLOR_MAGENTA, COLOR_BLACK)); print(KERNEL);
+                print_color("#CCCCCC/ \\CCCCCC#    ", MAKE_COLOR(COLOR_GREEN, COLOR_BLACK)); print("\n");
+                print_color("#CCCCC|   |CCCCC#    ", MAKE_COLOR(COLOR_GREEN, COLOR_BLACK)); print("\n");
+                print_color("#CCCCC|   |CCCCC#", MAKE_COLOR(COLOR_GREEN, COLOR_BLACK)); print("\n");
+                print_color("#################", MAKE_COLOR(COLOR_GREEN, COLOR_BLACK)); print("\n");
             } else if (buf_len > 0) {
                 print_color("\nERROR: Command not found", MAKE_COLOR(COLOR_RED, COLOR_BLACK));
             }
